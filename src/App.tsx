cd509f54d6c7496c0c6eb5c252253f8faab9f4fe
harutarooo <adgwtnp.3695@gmail.com>
2025-01-19 15:22:42 +0000

    文字の書き込み、保存（ローカル）を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,42 @@
-import { Note } from "./Note";
-import NoteList from "./NoteList";
+import { useState } from "react";
+// import { Note } from "./Note";
+// import NoteList from "./NoteList";
 import NoteEditor from "./NoteEditor";
 import "./App.css";
 
 function App() {
-  const notes: Note[] = [
-    {
-      id: 1,
-      title: "Note 1",
-      content: "Hello, World!",
-    },
-    {
-      id: 2,
-      title: "Note 2",
-      content: "Hello, React!",
-    },
-  ];
+  const [notes, setnotes] = useState<string>("");
 
-  const handleNewNote = () => {
-    // TODO: 処理を追加
+  const saveMd = () => {
+    if (notes.trim() !== "") {
+      const blob = new Blob([notes], { type: "text/markdown" });
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = "note.md";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      URL.revokeObjectURL(url);
+      // setnotes(""); // エディタをクリア
+    } else {
+      alert("内容を入力してください。");
+    }
   };
 
   return (
     <div className="flex w-screen">
-      <div className="w-[300px] bg-gray-100 p-4">
-        <div className="mb-4">
-          <button
-            className="w-full rounded bg-blue-500 p-2 font-bold text-white"
-            onClick={handleNewNote}
-          >
-            新規作成
-          </button>
-        </div>
-        <NoteList notes={notes} />
-      </div>
       <div className="flex-1 p-4">
         <div className="mb-4 flex justify-between">
           <h2 className="text-xl font-bold">Note Editor</h2>
-          <button className="rounded bg-green-500 p-2 font-bold text-white">
-            Preview
+          <button
+            className="rounded bg-green-500 p-2 font-bold text-white"
+            onClick={saveMd}
+          >
+            Save
           </button>
         </div>
-        <NoteEditor content={notes[0]?.content} />
+        <NoteEditor content={notes} onChange={setnotes} />
       </div>
     </div>
   );
--- src/NoteEditor.tsx
+++ src/NoteEditor.tsx
@@ -2,13 +2,17 @@ import React from "react";
 
 interface Props {
   content: string;
+  onChange: (content: string) => void;
 }
 
-const NoteEditor: React.FC<Props> = ({ content }) => {
+const NoteEditor: React.FC<Props> = ({ content, onChange }) => {
   return (
     <textarea
       className="h-[500px] w-full rounded border border-gray-300 p-2"
       value={content}
+      onChange={(e) => {
+        onChange(e.target.value);
+      }}
     />
   );
 };
