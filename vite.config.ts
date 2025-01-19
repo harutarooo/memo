ea09fbede3448e93d42a8b3f7f773af96f97e48a
harutarooo <adgwtnp.3695@gmail.com>
2025-01-19 12:41:40 +0000

    pwaの適用テスト

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: process.env.GITHUB_PAGES ? "memo" : "./",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["public/react.svg", "public/vite.svg"],
+      manifest: {
+        name: "PWA Memo",
+        short_name: "Memo",
+        description: "PWA Memo App",
+        theme_color: "#cccccc",
+        icons: [
+          {
+            src: "/public/react.svg",
+            sizes: "192x192",
+            type: "image/svg+xml",
+          },
+          {
+            src: "/public/vite.svg",
+            sizes: "512x512",
+            type: "image/svg+xml",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     host: "0.0.0.0",
     port: 3000,
